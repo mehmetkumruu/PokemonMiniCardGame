c93331e908bb14a13ae7eefc2e0544d51616f4d6
Meth <122786519+MethRAa@users.noreply.github.com>
2024-01-03 14:58:27 +0300

    [PK-01]-Player card created.

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// getpokemon(){
+//   fetch()
+//   pokemonData = await
+//   renderPlayerPokemon(pokemonData)
+// }
+
 // declare  const pi = 3.14;
 // assign   myNumb = 5;
 
@@ -15,7 +21,7 @@
 
 // console.log(myArry.length)
 
-const myArray = [];
+// const myArray = [];
 // const listNumber = Math.floor(Math.random() * 100);
 
 // for (let i = 0; i < 100; i++) {
@@ -31,9 +37,6 @@ const myArray = [];
 //     document.getElementById('random-number-p').innerHTML = myArray[randomNumber]
 // }
 
-function getRandomNumber() {
-  return Math.floor(Math.random() * 100);
-}
 // const changedArray = myArray.map((element) => {
 //     return `${element}'in Yüzle çarpılmış hali: ${element * 100}`;
 // })
@@ -47,61 +50,129 @@ function getRandomNumber() {
 //     console.log(data);
 // }
 
-let pokemon = {
+let playerPokemon = {
   id: "",
   name: "",
   initialHp: 0,
+  image: "",
   currentHp: 0,
   attack: 0,
   defense: 0,
+  race: "",
 };
 
 let enemy = {
   id: "",
   name: "",
-  hp: 0,
+  initialHp: 0,
+  image: "",
+  currentHp: 0,
   attack: 0,
   defense: 0,
+  race: "",
 };
 
-async function pokemonGetir() {
-  const randomId = getRandomNumber();
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
-  const pokemonData = await response.json();
-  console.log(pokemonData);
-
-  pokemon.name = pokemonData.name;
-  pokemon.attack = pokemonData.stats[1].base_stat;
-
-  // bunu başka fonskiyona al
-  const isPokeİmage = document.getElementById("poke-img");
-  const isPokeName = document.getElementById("poke-name");
-  const isPokeSpecies = document.getElementById("poke-species-name");
-  const isPokeWeight = document.getElementById("poke-weight");
-  isPokeİmage.src = pokemonData.sprites.front_default;
-  isPokeName.innerHTML = `Adı: ${pokemonData.name}`;
-  isPokeSpecies.innerHTML = `Tür: ${pokemonData.base_experience}`;
-  isPokeWeight.innerHTML = `Ağırlığı: ${pokemonData.weight}`;
-
-  console.log(pokemon);
-}
-
 function getRandomNumber() {
-  const randomDecimal = Math.random();
-  const randomNumber = Math.floor(randomDecimal * 3) + 1;
-  return randomNumber;
+  return Math.floor(Math.random() * 100);
+}
+// async function pokemonPlayerGetir() {
+//   const
+// }
+async function pokemonEnemyGetir() {
+  const randomId = getRandomNumber();
+  const responseEnemy = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${randomId}`
+  );
+  const pokemonDataEnemy = await responseEnemy.json();
+  const responsePlayer = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${randomId}`
+  );
+  const pokemonUser = await responsePlayer.json();
+  const pokeRace = pokemonUser.types.map((pokemonRace) => {
+    return pokemonRace.type.name;
+  });
+  // for (let index = 0; index < pokeRace.length; index++) {
+  //   if (pokeRace[index] === "undefined") {
+  //     pokeRace;
+  //   }
+  // }
+  console.log(pokemonUser.types);
+  playerPokemon.name = pokemonUser.name;
+  playerPokemon.initialHp = pokemonUser.stats[0].base_stat;
+  playerPokemon.image = pokemonUser.sprites.other.home.front_default;
+  playerPokemon.attack = pokemonUser.stats[1].base_stat;
+  playerPokemon.defense = pokemonUser.stats[2].base_stat;
+  playerPokemon.race = pokeRace;
+  renderPlayerPokemon(playerPokemon);
+  // playerPokemon.attack = pokemonDataPlayer.
+  // console.log(pokemonDataEnemy);
+  // console.log(pokemonDataPlayer);
+  console.log(playerPokemon.name);
+  // bunu başka fonskiyona al
+  // const isPokeİmage = document.getElementById("poke-img");
+  // const isPokeName = document.getElementById("poke-name");
+  // const isPokeSpecies = document.getElementById("poke-species-name");
+  // const isPokeWeight = document.getElementById("poke-weight");
+  // isPokeİmage.src = pokemonData.sprites.front_default;
+  // isPokeName.innerHTML = `Adı: ${pokemonData.name}`;
+  // isPokeSpecies.innerHTML = `Tür: ${pokemonData.base_experience}`;
+  // isPokeWeight.innerHTML = `Ağırlığı: ${pokemonData.weight}`;
 }
 
-function performAttack(attacker, defender) {
-  const currentAttack = attacker.attack * getRandomNumber() - defender.defense;
-  defender.hp -= currentAttack;
+function renderPlayerPokemon(pokemon) {
+  const userCard = document.getElementById("pokemon-game-card");
+  userCard.innerHTML = `
+  <div class="poke-card-info">
+  <img class="poke-card-rank" src="" alt="">
+  <h2 class="poke-hero-name">${pokemon.name}</h2>
+  <h1 id="poke-hero-hp" class="poke-hero-hp">${pokemon.initialHp}</h1>
+</div>
+<div class="poke-card-hero-image">
+  <img id="poke-hero-img" class="poke-hero-img" src="${pokemon.image}" alt="">
+</div>
+<div class="poke-card-power-info">
+  <div class="poke-hero-info">
+      <div class="poke-hero-gen-info">  
+          <p id="poke-hero-race" class="poke-hero-race">RACE  <span>${
+            pokemon.race[0]
+          } ${pokemon.race[1] ?? ""}</span></p>
+      </div>
+      <div class="poke-hero-defense-info">
+          <p class="poke-hero-defense">DEFENSE:<strong>${
+            pokemon.defense
+          }</strong></p>
+      </div>
+  </div>
+  <div class="poke-hero-info-down-side">
+    <div class="poke-hero-update-power-img">
+        <a class="poke-attack-up"><img class="poke-hero-attack-up" src="" alt=""></a>
+        <a class="poke-defence-up"><img class="poke-hero-defence-up" src="" alt=""></a>
+    </div>
+    <div class="poke-hero-attack-info">
+        <p class="poke-attack-text">ATTACK :</p>
+        <h1 class="poke-attack-info">${pokemon.attack}</h1>
+    </div>
+  </div>
+</div>
+  `;
 }
+pokemonEnemyGetir();
+// function getRandomNumber() {
+//   const randomDecimal = Math.random();
+//   const randomNumber = Math.floor(randomDecimal * 3) + 1;
+//   return randomNumber;
+// }
 
-function checkIsPokemonDead(){
-    if(pokemon.hp <= 0){
-        // karakterin öldüğü mantık
-    }
-}
+// function performAttack(attacker, defender) {
+//   const currentAttack = attacker.attack * getRandomNumber() - defender.defense;
+//   defender.hp -= currentAttack;
+// }
+
+// function checkIsPokemonDead(){
+//     if(pokemon.hp <= 0){
+//         // karakterin öldüğü mantık
+//     }
+// }
 
 // performAttack(pokemon, enemy);
 // performAttack(pokemon, enemy);
